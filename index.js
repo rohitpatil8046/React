644b74b1e8988357fa6e65e102596c08e732f494
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-28 19:39:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-
-let curDate = new Date(2021,5,5,14);
-curDate = curDate.getHours();
-let greeting = '';
-const cssStyle = { };
-
-if(curDate >= 1 && curDate < 12){
-  greeting = 'Good Morning';
-  cssStyle.color = 'green';
-}else if(curDate>12 && curDate <19){
-  greeting = 'Good Afternoon';
-  cssStyle.color = 'orange';
-}else{
-  greeting = 'Good Night';
-  cssStyle.color = 'black';
-}
+import Card from "./Cards";
 
 ReactDOM.render(
-  <>
-  <div>
-  <h1>Hello Sir, <span style={cssStyle}> {greeting}</span></h1>,
-  </div>
-  </>,
-  document.getElementById('root')
+<>
+<Card imgsrc="https://www.google.com/imgres?imgurl=https%3A%2F%2Fd2rdhxfof4qmbb.cloudfront.net%2Fwp-content%2Fuploads%2F20200813125652%2Fdark-1.jpg&imgrefurl=https%3A%2F%2Ftravel.earth%2Fnetflix-show-dark-filming-locations%2F&tbnid=4zE3PIPZ0f-HKM&vet=12ahUKEwiB_o_46M_4AhU-LrcAHXqjDtwQMygFegUIARDjAQ..i&docid=RZBduBMJSeVDxM&w=712&h=315&q=dark%20netflix&ved=2ahUKEwiB_o_46M_4AhU-LrcAHXqjDtwQMygFegUIARDjAQ"
+title="A Netflix Originl Series"
+sname="Dark"
+link="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmiro.medium.com%2Fmax%2F960%2F1*BIVheiO7AsrxHwokkmokWQ.jpeg&imgrefurl=https%3A%2F%2Fmedium.com%2Fillumination%2Fimprove-your-life-with-these-lessons-from-netflixs-dark-c61df7a1e8d8&tbnid=2TKCPFXlAmoNgM&vet=12ahUKEwiB_o_46M_4AhU-LrcAHXqjDtwQMygEegUIARDhAQ..i&docid=HMLS6eHPuU-XqM&w=960&h=540&q=dark%20netflix&ved=2ahUKEwiB_o_46M_4AhU-LrcAHXqjDtwQMygEegUIARDhAQ"
+/> 
+<Card imgsrc="https://www.google.com/imgres?imgurl=https%3A%2F%2Fd2rdhxfof4qmbb.cloudfront.net%2Fwp-content%2Fuploads%2F20200813125652%2Fdark-1.jpg&imgrefurl=https%3A%2F%2Ftravel.earth%2Fnetflix-show-dark-filming-locations%2F&tbnid=4zE3PIPZ0f-HKM&vet=12ahUKEwiB_o_46M_4AhU-LrcAHXqjDtwQMygFegUIARDjAQ..i&docid=RZBduBMJSeVDxM&w=712&h=315&q=dark%20netflix&ved=2ahUKEwiB_o_46M_4AhU-LrcAHXqjDtwQMygFegUIARDjAQ"
+title="A Netflix Originl Series"
+sname="Money Heist"
+link="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmiro.medium.com%2Fmax%2F960%2F1*BIVheiO7AsrxHwokkmokWQ.jpeg&imgrefurl=https%3A%2F%2Fmedium.com%2Fillumination%2Fimprove-your-life-with-these-lessons-from-netflixs-dark-c61df7a1e8d8&tbnid=2TKCPFXlAmoNgM&vet=12ahUKEwiB_o_46M_4AhU-LrcAHXqjDtwQMygEegUIARDhAQ..i&docid=HMLS6eHPuU-XqM&w=960&h=540&q=dark%20netflix&ved=2ahUKEwiB_o_46M_4AhU-LrcAHXqjDtwQMygEegUIARDhAQ" 
+/> 
+
+<Card imgsrc="https://www.google.com/imgres?imgurl=https%3A%2F%2Fd2rdhxfof4qmbb.cloudfront.net%2Fwp-content%2Fuploads%2F20200813125652%2Fdark-1.jpg&imgrefurl=https%3A%2F%2Ftravel.earth%2Fnetflix-show-dark-filming-locations%2F&tbnid=4zE3PIPZ0f-HKM&vet=12ahUKEwiB_o_46M_4AhU-LrcAHXqjDtwQMygFegUIARDjAQ..i&docid=RZBduBMJSeVDxM&w=712&h=315&q=dark%20netflix&ved=2ahUKEwiB_o_46M_4AhU-LrcAHXqjDtwQMygFegUIARDjAQ"
+title="A Netflix Originl Series"
+sname="Stranger Things"
+link="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmiro.medium.com%2Fmax%2F960%2F1*BIVheiO7AsrxHwokkmokWQ.jpeg&imgrefurl=https%3A%2F%2Fmedium.com%2Fillumination%2Fimprove-your-life-with-these-lessons-from-netflixs-dark-c61df7a1e8d8&tbnid=2TKCPFXlAmoNgM&vet=12ahUKEwiB_o_46M_4AhU-LrcAHXqjDtwQMygEegUIARDhAQ..i&docid=HMLS6eHPuU-XqM&w=960&h=540&q=dark%20netflix&ved=2ahUKEwiB_o_46M_4AhU-LrcAHXqjDtwQMygEegUIARDhAQ"/> 
+</>,
+
+document.getElementById('root')
+
 );
+
+
 
